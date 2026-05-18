a38584412006f9a9a2406b78c7af5c70f2f812fe
aoba.f <aoba200608@gmail.com>
2026-05-18 12:00:31 +0900

    chore: コメント追加

--- week3/php-practice/comment_fixed.php
+++ week3/php-practice/comment_fixed.php
@@ -17,15 +17,19 @@
 </form>
 
 <?php
+// フォームが送信されたときの処理
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $name    = $_POST["name"];
     $comment = $_POST["comment"];
 
-   if ($name == "") {
+    // nameとcommentの両方が入力されているかを確認し、入力されていない場合はエラーメッセージを表示
+    if ($name == "") {
         echo "名前を入力してください。";
     } elseif ($comment == "") {
         echo "コメントを入力してください。";
-    } else {
+    } 
+    //  両方とも入力されている場合は、名前とコメントを安全に表示
+     else {
         echo "<p>" . htmlspecialchars($name, ENT_QUOTES, 'UTF-8') . "さんのコメント:</p>";
         echo "<p>" . htmlspecialchars($comment, ENT_QUOTES, 'UTF-8') . "</p>";
     }
